c399515dd5f51fb866cf0c57215397184bb4fd96
Theodore Mauros <akos@pop-os.localdomain>
2026-05-26 13:36:45 +0300

    update site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Ellie Tattooer",
+  title: "★ ELLIE TATTOOER ★",
   description: "American Traditional Tattooing",
 };
 
